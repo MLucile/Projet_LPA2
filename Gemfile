5e0a12670cea91ddcaa405028e5f60e6dab254a6
mlucile.08 <mlucile.08@gmail.com>
2017-12-07 12:49:55 -0500

    Mise en place de VCR avec suppression des warnings

--- Gemfile
+++ Gemfile
@@ -42,3 +42,5 @@ gem 'spring',        group: :development
 gem 'nominatim', '~> 0.0.6'
 
 gem 'forecast_io', '~> 2.0.1'
+
+gem 'vcr', '~> 3.0.3', group: [:development, :test]
--- app/models/town.rb
+++ app/models/town.rb
@@ -1,11 +1,18 @@
 class Town < ActiveRecord::Base
   
   before_validation :load_position
+  validates :lat, :lon, presence: true
+  
+  def get_weather()
+    result = ForecastIO.forecast(self.lat, self.lon)
+  end
   
   private
   def load_position
-    places = Nominatim.search(name).limit(1)
-    self.lat = places.first.lat
-    self.lon = places.first.lon
+    places = Nominatim.search(name).limit(1).first
+    if places
+      self.lat = places.lat
+      self.lon = places.lon
+    end
   end
 end
